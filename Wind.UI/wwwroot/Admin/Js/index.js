d123566c6a7f5462eb499a86c53c4324a13127eb
17576052526 <727863984@qq.com>
2020-12-17 17:52:20 +0800

    -

---
+++ Wind.UI/wwwroot/Admin/Js/index.js
@@ -0,0 +1,19 @@
+﻿//树结构显示隐藏
+$(document).on('click', '.tree-switch', function () {
+    var box = $(this).closest('.tree');
+    var child = box.children('.tree-box');
+    if (box.hasClass('tree-active')) {
+        //当前隐藏
+        child.css('display', 'block');
+        box.removeClass('tree-active');
+        child.slideUp(300);
+    } else {
+        //其他显示的隐藏
+        var active = box.parent().children('.tree-active');
+        active.children('.tree-box').css('display', 'block').slideUp(300);
+        active.removeClass('tree-active');
+        //当前显示
+        child.slideDown(300);
+        box.addClass('tree-active');
+    }
+});
