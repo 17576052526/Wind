62e35943a0a44115c745a54bd236028636b0bea3
17576052526 <727863984@qq.com>
2020-12-21 13:44:43 +0800

    -

--- Wind.UI/wwwroot/Admin/Js/index.js
+++ Wind.UI/wwwroot/Admin/Js/index.js
@@ -26,34 +26,46 @@ $(document).on('click','.left-switch', function () {
         cur.css('display', 'none');
     }
 });
-//左边搜索框搜索事件  .tree-search 输入框，.tree 树的项，.tree-box 子级的父级，tree-active 打开，注意：不能在节点用style 属性设置display，如果要设置显示隐藏请用类样式设置
+//树结构搜索插件
 $(document).on('focus', '.tree-search', function () {
     function search(str) {
         $('.tree').each(function () {
             var $this = $(this);
             var box = $this.children('.tree-box');
-            var che = box.children(':contains("' + str + '")');
-            if (che.length > 0) {
-                $this.css('display', '').addClass('tree-active');
-                box.css('display', '');
-                che.css('display', '');
-                box.children().not(che).css('display', 'none');
+            var che_title = $this.children(':not(".tree-box"):contains("' + str + '")');
+            var che_chil = box.children(':contains("' + str + '")');
+            if (che_title.length > 0 || che_chil.length > 0) {
+                if (che_title.length > 0) {
+                    $this.css('display', '').removeClass('tree-active');
+                    box.css('display', '');
+                    box.children().css('display', '');
+                }
+                if (che_chil.length > 0) {
+                    $this.css('display', '').addClass('tree-active');
+                    box.css('display', '');
+                    che_chil.css('display', '');
+                    box.children().not(che_chil).css('display', 'none');
+                }
             } else {
                 $this.css('display', 'none');
             }
         });
     }
     var cur = $(this);
-    function keyup() {
-        var val = cur.val();
-        search(val);
-        if (val.length == 0) {
-            $('.tree').removeClass('tree-active');
-        }
+    var timer = null;
+    function keydown() {
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            var val = cur.val();
+            search(val);
+            if (val.length == 0) {
+                $('.tree').removeClass('tree-active');
+            }
+        }, 300)
     }
     //清除
-    function blur() { cur.off({ 'blur': blur, 'keyup': keyup }); }
-    cur.on({ 'blur': blur, 'keyup': keyup });
+    function blur() { cur.off({ 'blur': blur, 'keydown': keydown }); }
+    cur.on({ 'blur': blur, 'keydown': keydown });
     //输入框选中文本，IE11不支持
     this.setSelectionRange(0, this.value.length);
 });
