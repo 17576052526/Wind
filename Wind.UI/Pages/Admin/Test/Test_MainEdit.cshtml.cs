08714eedac94f342973271a6315827414ef45d78
17576052526 <727863984@qq.com>
2021-03-17 20:40:05 +0800

    -

--- Wind.UI/Pages/Admin/Test/Test_MainEdit.cshtml.cs
+++ Wind.UI/Pages/Admin/Test/Test_MainEdit.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DbOrm;
 using DbOrm.Model;
@@ -21,17 +22,29 @@ namespace Wind.UI.Pages.Admin.Test
         }
 
         //新增或修改
-        public void OnPost(Test_Main model)
+        public IActionResult OnPost(Test_Main model,string id)
         {
-            if (model.ID == null)
+            if (id == null)
             {
                 DB.Insert(model);
             }
             else
             {
-                DB.Update(model, "ID=@ID");
+                //为 null 不参与修改，所以一定要有值才会参与修改
+                if (model.MainID == null) { model.MainID = ""; }
+                if (model.MainName == null) { model.MainName = ""; }
+                if (model.Test_Type_ID == null) { model.Test_Type_ID = 0; }
+                if (model.Quantity == null) { model.Quantity = 0; }
+                if (model.Amount == null) { model.Amount = 0; }
+                if (model.IsShow == null) { model.IsShow = false; }
+                if (model.Img == null) { model.Img = ""; }
+                if (model.Files == null) { model.Files = ""; }
+                if (model.Remark == null) { model.Remark = ""; }
+                if (model.CreateTime == null) { model.CreateTime = Convert.ToDateTime("1/1/1753 12:00:00"); }
+
+                DB.Update(model, "ID=@ID", new { ID = id });
             }
-            Response.Redirect("List");
+            return Redirect(Regex.Match(Request.QueryString.Value, "(?<=url=).+", RegexOptions.IgnoreCase).Value);
         }
     }
 }
