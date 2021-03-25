2746a8c65e3ddbc8feeebbcaccef47648ad2abfd
17576052526 <727863984@qq.com>
2021-03-25 11:41:49 +0800

    -

--- Wind.UI/Pages/Admin/EditPageModel.cs
+++ Wind.UI/Pages/Admin/EditPageModel.cs
@@ -12,7 +12,6 @@ namespace Wind.UI.Pages.Admin
     /// 编辑页基类（公共部分写到此类当中，例如：权限控制，判断是否有登录）
     /// </summary>
     [Authorize]//验证是否有登录
-    //[ValidateInput(false)]  //用于不验证富文本控件提交过来的 html字符串
     public class EditPageModel: PageModel
     {
 
