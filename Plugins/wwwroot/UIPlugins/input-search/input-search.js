9d78e3135d8e4565a96b8fc9ebd07c37778275f4
17576052526 <727863984@qq.com>
2021-05-08 15:51:02 +0800

    -

--- Plugins/wwwroot/UIPlugins/input-search/input-search.js
+++ Plugins/wwwroot/UIPlugins/input-search/input-search.js
@@ -1,26 +1,51 @@
-﻿+function ($) {
-    //url:提交的url,输入框的value是直接跟在url后面的，jsonFun：json解析方法，selectClickFun：下拉点击事件
-    $.fn.inputSearch = function (url, jsonFun, selectClickFun) {
-        this.on('blur', function () { $('#__inputSearch4725').css('display', 'none'); });
-        this.on('focus', function () { $('#__inputSearch4725').css('display', 'block'); });
-        this.on('keyup', function () {
-            if (this.value.length == 0) { $('#__inputSearch4725').remove(); return; }
-            var cur = $(this);
-            $.ajax({
-                type: "get",
-                url: url + this.value,
-                success: function (msg) {
-                    var arr = jsonFun(msg);
-                    $('#__inputSearch4725').remove();
-                    var ul = $('<ul class="inputSearchBox" id="__inputSearch4725" style="position:absolute;left:' + cur.offset().left + 'px;top:' + (cur.offset().top + cur.outerHeight()) + 'px;width:' + cur.outerWidth() + 'px"></ul>');
-                    for (var m of arr) {
-                        ul.append('<li value="' + m.value + '">' + m.text.replace(cur.val(), function (s) { return '<span>' + s + '</span>'; }) + '</li>');
+﻿$.fn.inputSearch = function (settings) {
+    var param = $.extend({
+        type: 'get',   //ajax请求类型 get，post
+        data: null,    //ajax请求参数
+        url: null,    //ajax请求路径
+        toJson: null,//返回的数据转换成指定格式的json
+        dropClick: null//点击下拉触发的事件
+    }, settings || {});
+
+    var cur = this;
+    var timer = null;
+
+    function search() {
+        var val = cur.val();
+        if (val.length == 0) { return; }
+        $.ajax({
+            type: param.type,
+            url: param.url,
+            data: param.data,
+            success: function (msg) {
+                $('#__inputSearch4725').remove();
+                var arr = param.toJson(msg);
+                if (arr.length > 0) {
+                    var box = $('<ul class="inputSearchBox" id="__inputSearch4725" style="position:absolute;left:' + cur.offset().left + 'px;top:' + (cur.offset().top + cur.outerHeight()) + 'px;min-width:' + cur.outerWidth() + 'px"></ul>').appendTo(document.body);
+                    for (var i = 0; i < arr.length; i++) {
+                        box.append('<li value="' + arr[i].value + '">' + arr[i].text.replace(val, function (s) { return '<span>' + s + '</span>'; }) + '</li>');
                     }
-                    ul.children().on('mousedown', function () { $('#__inputSearch4725').remove(); cur.val($(this).text()); cur.attr('val', $(this).attr('value')); selectClickFun(); });//绑定事件
-                    $('body').append(ul);
-                },
-                error: function () { alert('请求失败'); }
-            })
-        })
+                    box.on('mousedown', 'li', function () {//此处mousedown 代替click
+                        var val = $(this).attr('value');
+                        var text = $(this).text();
+                        cur.attr('val', val);
+                        cur.val(text);
+                        param.dropClick(val, text);
+                    })
+                }
+            }
+        });
     }
-}(jQuery)
+    function keydown() {
+        clearTimeout(timer);
+        timer = setTimeout(search, 300);
+    }
+    function blur() {
+        $('#__inputSearch4725').remove();
+        cur.off({ 'keydown': keydown, 'blur': blur });
+    }
+    this.on('focus', function () {
+        search();
+        cur.on({ 'keydown': keydown, 'blur': blur });
+    })
+}
