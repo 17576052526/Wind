c706aae8e69022e1ce8ad68ad9b8cf85295ea1b6
17576052526 <727863984@qq.com>
2021-05-10 11:18:49 +0800

    -

--- Plugins/wwwroot/UIPlugins/loading/loading.js
+++ Plugins/wwwroot/UIPlugins/loading/loading.js
@@ -4,16 +4,16 @@
     $(box).loadingStop()：停止 box里面的 loading图显示
 */
 +function ($) {
-    var timer;
+    var _loading_timer;
     $.fn.loading = function (interval) {
         var cur = this;
-        timer = setTimeout(function () {
-            cur.append('<div class="fixed-center-box"><div class="loading1"><span></span><span></span><span></span><span></span><span></span></div></div>');
+        _loading_timer = setTimeout(function () {
+            cur.append('<div class="loading-box"><div class="loading1"><span></span><span></span><span></span><span></span><span></span></div></div>');
             if (cur.css('position') == 'static') { cur.css('position', 'relative') }
         }, interval || 2000);
     }
     $.fn.loadingStop = function () {
         this.children('.fixed-center-box').remove();
-        clearTimeout(timer);
+        clearTimeout(_loading_timer);
     }
 }(jQuery)
