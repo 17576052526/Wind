3ca8dacda305cff8a16d34f0a237e0492ac2acbf
17576052526 <727863984@qq.com>
2021-05-12 19:00:10 +0800

    -

--- Plugins/wwwroot/UIPlugins/ctrl-mouse/ctrl-mouse.js
+++ Plugins/wwwroot/UIPlugins/ctrl-mouse/ctrl-mouse.js
@@ -3,32 +3,36 @@
  * 参数一：按住的节点，参数二：接收的节点，参数三：接收事件
  */
 $.ctrlMouse = function (downNode, upNode, upEvent) {
-    $(document).on('keydown', function (e) {
-        if (e.keyCode == 17) {//按下ctrl
-            function keyup(e) {
-                if (e.keyCode == 17) {
-                    $(document).off('keyup', keyup);//释放弹开事件
-                    $(downNode).off('mousedown', downNodeMousedown);//释放节点按下事件
-                }
-            }
-            function downNodeMousedown() {
-                var downObj = this;
-                function mouseup() {
-                    $(document).off('mouseup', mouseup);//释放
-                    $(upNode).off('mouseup', upNodeMouseup);//释放
+    var isCtrl = false;
+    var downObj;
+    $(document).on('keydown', function (e) { if (e.keyCode == 17) { isCtrl = true; } });
+    $(document).on('keyup', function (e) { if (e.keyCode == 17) { isCtrl = false; } });
+    $(document).on('mousedown', downNode, function () {
+        if (isCtrl) {
+            downObj = this;
+            $(downNode).css('cursor', 'move');
+            $(document.body).css({ 'user-select': 'none', 'cursor': 'move' });
+            $(upNode).css('cursor', 'alias');
+            //释放资源
+            function mouseup() {
+                if (downObj) {
+                    downObj = null;
                     $(downNode).css('cursor', '');
                     $(document.body).css({ 'user-select': '', 'cursor': '' });
                     $(upNode).css('cursor', '');
+                    //释放事件
+                    $(document).off('mouseup', mouseup);
+                    $(document).off('mouseup', upNodeMouseup);
                 }
-                function upNodeMouseup() { upEvent(downObj, this); }
-                $(document).on('mouseup', mouseup);//绑定
-                $(upNode).on('mouseup', upNodeMouseup);//绑定
-                $(downNode).css('cursor', 'move');
-                $(document.body).css({ 'user-select': 'none', 'cursor': 'move' });
-                $(upNode).css('cursor', 'alias');
             }
-            $(document).on('keyup', keyup);//绑定弹开事件
-            $(downNode).on('mousedown', downNodeMousedown);//绑定节点按下事件
+            function upNodeMouseup() {
+                if (downObj) {
+                    upEvent(downObj, this);
+                }
+            }
+            //绑定
+            $(document).on('mouseup', mouseup);
+            $(document).on('mouseup', upNode, upNodeMouseup);
         }
     });
 }
