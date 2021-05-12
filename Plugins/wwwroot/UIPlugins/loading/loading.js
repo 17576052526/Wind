90593ee5252d200bc292da679e80f74f878553f0
17576052526 <727863984@qq.com>
2021-05-12 11:07:03 +0800

    -

--- Plugins/wwwroot/UIPlugins/loading/loading.js
+++ Plugins/wwwroot/UIPlugins/loading/loading.js
@@ -4,16 +4,16 @@
     $(box).loadingStop()：停止 box里面的 loading图显示
 */
 +function ($) {
-    var _loading_timer;
+    var _loading_timer = [];
     $.fn.loading = function (interval) {
         var cur = this;
-        _loading_timer = setTimeout(function () {
+        _loading_timer.push(setTimeout(function () {
             cur.append('<div class="loading-box"><div class="loading1"><span></span><span></span><span></span><span></span><span></span></div></div>');
             if (cur.css('position') == 'static') { cur.css('position', 'relative') }
-        }, interval || 2000);
+        }, interval || 2000));
     }
     $.fn.loadingStop = function () {
-        this.children('.loading-box').remove();
-        clearTimeout(_loading_timer);
+        this.children('.loading-box').eq(0).remove();
+        if (_loading_timer.length > 0) { clearTimeout(_loading_timer[0]); _loading_timer.splice(0, 1); }
     }
 }(jQuery)
