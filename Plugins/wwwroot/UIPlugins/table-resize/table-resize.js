5ebc9178fff732ddacc8e376906c0250020b2980
17576052526 <727863984@qq.com>
2021-05-14 16:01:09 +0800

    -

--- Plugins/wwwroot/UIPlugins/table-resize/table-resize.js
+++ Plugins/wwwroot/UIPlugins/table-resize/table-resize.js
@@ -5,8 +5,8 @@
     //鼠标按下
     $(document).on('mousedown', '.table-resize-move', function (e) {
         var th = $(this).parent();
-        var resizeBoxs = th.closest('table').find('tr').find('th:eq(' + th.index() + ')>.table-resize-item,td:eq(' + th.index() + ')>.table-resize-item');
         var x = e.clientX - th.width();//记录按下的位置
+        var resizeBoxs = th.closest('table').find('tr').find('th:eq(' + th.index() + ')>.table-resize-item,td:eq(' + th.index() + ')>.table-resize-item');
         $(document.body).css({ 'user-select': 'none', 'cursor': 'col-resize' });
         //鼠标移动
         function mousemove(e) {
@@ -20,5 +20,9 @@
         $(document).on({ 'mousemove': mousemove, 'mouseup': mouseup });
     });
     //添加节点
-    $('.table-resize').append('<span class="table-resize-move"></span>');
+    $(document).on('mouseenter', '.table-resize', function () {
+        if ($(this).children('.table-resize-move').length == 0) {
+            $(this).append('<span class="table-resize-move"></span>');
+        }
+    });
 }(jQuery)
