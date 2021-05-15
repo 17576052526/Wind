8471052323e67597364431b24044f68880e13405
17576052526 <727863984@qq.com>
2021-05-15 09:13:53 +0800

    -

---
+++ Plugins/wwwroot/UIPlugins/box-move/box-move.js
@@ -0,0 +1,30 @@
+﻿/*
+ 鼠标移动div
+    .box-move 是要移动的框
+    .box-move-switch 鼠标按下的块
+ */
+$(document).on('mousedown', '.box-move-switch', function (e) {
+    var box = $(this).closest('.box-move');
+    //当前元素是否是正常文档流还是定位
+    var x, y;
+    if (box.css('position') == 'static') {
+        //获取节点位置
+        var offset = box.offset();
+        x = e.clientX - offset.left;
+        y = e.clientY - offset.top;
+        box.css({ 'position': 'absolute', 'left': offset.left, 'top': offset.top });
+    } else {
+        x = e.clientX - parseInt(box.css('left'));
+        y = e.clientY - parseInt(box.css('top'));
+    }
+    //设置body
+    $(document.body).css({ 'user-select': 'none', 'cursor': 'move' });
+    //鼠标移动
+    function mousemove(e) { box.css({ 'left': e.clientX - x, 'top': e.clientY - y }); }
+    //鼠标松开
+    function mouseup(e) {
+        $(document.body).css({ 'user-select': '', 'cursor': '' });
+        $(document).off({ 'mousemove': mousemove, 'mouseup': mouseup });
+    }
+    $(document).on({ 'mousemove': mousemove, 'mouseup': mouseup });
+});
