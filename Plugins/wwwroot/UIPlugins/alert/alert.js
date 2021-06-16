75d0cbb4e6f806758a86e340c15265383269a409
17576052526 <727863984@qq.com>
2021-06-16 18:01:16 +0800

    -

--- Plugins/wwwroot/UIPlugins/alert/alert.js
+++ Plugins/wwwroot/UIPlugins/alert/alert.js
@@ -5,7 +5,8 @@
 $.alert = function (msg) {
     var box = $('#__alert372');
     if (box.length == 0) { box = $('<div class="alert-box" id="__alert372"></div>').appendTo(document.body); }
-    var msgBox = $('<div class="alert"><div style="max-height:75vh;overflow:auto;"><div class="alert-remove" onclick="$(this).closest(\'.alert\').remove()">+</div>' + msg + '</div></div><br>').appendTo(box);//<br>用于换行
+    var msgBox = $('<div class="alert"><div style="max-height:75vh;overflow:auto;">' + msg + '</div></div><br>').appendTo(box);//<br>用于换行
+    $('<div class="alert-remove">+</div>').appendTo(msgBox.children()).on('click', function () { $(this).closest('.alert').remove() });
     function createTime() { return setTimeout(function () { msgBox.addClass('alert-hide'); setTimeout(function () { msgBox.remove() }, 500); }, 5000); }
     var times = createTime();//创建计时器
     msgBox.hover(function () { clearTimeout(times); }, function () { times = createTime(); });
