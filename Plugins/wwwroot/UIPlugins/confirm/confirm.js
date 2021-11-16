5573fd7061ebb1709c21a9f66547ff4095974727
17576052526 <727863984@qq.com>
2021-11-16 14:03:34 +0800

    -

--- Plugins/wwwroot/UIPlugins/confirm/confirm.js
+++ Plugins/wwwroot/UIPlugins/confirm/confirm.js
@@ -10,14 +10,16 @@ $.confirm = function (content, yesFunc, title, noFunc) {
     $(document.body).append('<div style="position:fixed;left:0px;top:0px;z-index:100; width:100%;height:100%;" id="confirm_394"></div>');
     //添加 box框
     var box = $('<div class="box box-move confirm" id="confirm_815">\
-            <div class="box-head box-move-switch" style="display:flex;justify-content:space-between;"><div>'+ (title || '消息提示') + '</div><span class="confirm-remove" onclick="$(\'#confirm_394,#confirm_815\').remove()">+</span></div>\
             <div class="box-body">'+ content + '</div>\
         </div>').appendTo(document.body);
     var boxFoot = $('<div class="box-foot"></div>').appendTo(box);
     $('<input type="button" value="确定" class="btn-primary" />').appendTo(boxFoot).on('click', function () {
-        $('#confirm_394,#confirm_815').remove(); yesFunc();
+        $('#confirm_394,#confirm_815').remove(); yesFunc && yesFunc();
     });
     $('<input type="button" value="取消" class="btn-default" style="margin-left:15px;" />').appendTo(boxFoot).on('click', function () {
-        $('#confirm_394,#confirm_815').remove(); noFunc();
+        $('#confirm_394,#confirm_815').remove(); noFunc && noFunc();
     });
+    //关闭弹出框react 中不能直接在dome中加onclick,所以改用jquery绑定
+    var boxHead = $('<div class="box-head box-move-switch" style="display:flex;justify-content:space-between;"><div>' + (title || '消息提示') + '</div></div>').prependTo(box)
+    $('<span class="confirm-remove">+</span>').appendTo(boxHead).on('click', function () { $('#confirm_394,#confirm_815').remove() })
 }
