6a769155bdc74b3d9d6568774da73107731f7376
17576052526 <727863984@qq.com>
2022-08-03 15:59:58 +0800

    -

--- React/src/common.js
+++ React/src/common.js
@@ -3,11 +3,12 @@
 let common = {
     //获取浏览器缓存
     getCache: function (name) {
-        return window.localStorage.getItem(name);
+        return JSON.parse(window.localStorage.getItem(name));
     },
     //写入浏览器缓存
     setCache: function (name, value) {
-        return window.localStorage.setItem(name, value);
+        if (!(value instanceof Object)) { alert('common.setCache() 只能存入对象或数组，不能存基础数据类型'); }//因为取出的时候，判断不了是基础数据类型还是引用数据类型
+        return window.localStorage.setItem(name, JSON.stringify(value));
     }
 }
 export default common;
