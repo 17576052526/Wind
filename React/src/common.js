c101ca8e2c448c6cce2208b13bb0482f445802a9
17576052526 <727863984@qq.com>
2022-08-10 16:02:24 +0800

    -

--- React/src/common.js
+++ React/src/common.js
@@ -1,14 +1,41 @@
 //import $ from 'jquery';
 
-let common = {
-    //获取浏览器缓存
-    getCache: function (name) {
-        return JSON.parse(window.localStorage.getItem(name));
+export default {
+    //设置 localStorage
+    setLocalStorage: (name, value) => {
+        if (value instanceof Object) {
+            window.localStorage.setItem(name, JSON.stringify(value));
+        } else {
+            window.localStorage.setItem(name, value);
+        }
     },
-    //写入浏览器缓存
-    setCache: function (name, value) {
-        if (!(value instanceof Object)) { alert('common.setCache() 只能存入对象或数组，不能存基础数据类型'); }//因为取出的时候，判断不了是基础数据类型还是引用数据类型
-        return window.localStorage.setItem(name, JSON.stringify(value));
-    }
+    //获取 localStorage
+    getLocalStorage: (name) => {
+        let val = window.localStorage.getItem(name);
+        try {
+            return JSON.parse(val);
+        }
+        catch {
+            return val;
+        }
+    },
+
+    //设置 sessionStorage
+    setSessionStorage: (name, value) => {
+        if (value instanceof Object) {
+            window.sessionStorage.setItem(name, JSON.stringify(value));
+        } else {
+            window.sessionStorage.setItem(name, value);
+        }
+    },
+    //获取 sessionStorage
+    getSessionStorage: (name) => {
+        let val = window.sessionStorage.getItem(name);
+        try {
+            return JSON.parse(val);
+        }
+        catch {
+            return val;
+        }
+    },
 }
-export default common;
