cd6388ec13518116626f8909c7ef16e79b429a38
17576052526 <727863984@qq.com>
2022-08-11 16:31:22 +0800

    -

--- React/src/components/login.js
+++ React/src/components/login.js
@@ -1,27 +1,73 @@
 import React, { useState, useEffect, useContext, useRef } from 'react';
+import { useNavigate } from "react-router-dom";
 import axios from 'axios'
 import $ from 'jquery'
 import '../css/global.css'
 import '../css/font/css/fontello.css'
+import common from '../common'
 
 export default function () {
-
+    let navigate = useNavigate();
+    let userName = useRef();
+    let userPwd = useRef();
+    let verifyCode = useRef();
+    let verifyImg = useRef();
+    let remember = useRef();
+    let [isVerifyCode, setIsVerifyCode] = useState();
 
     useEffect(() => {
+        //记住密码，读取信息
+        if (common.getLocalStorage("rememberUserName")) {
+            userName.current.value = common.getLocalStorage("rememberUserName");
+            userPwd.current.value = common.getLocalStorage("rememberUserPwd");
+            remember.current.checked = true;
+            userName.current.focus();
+        }
+    }, [])
 
-    }, []);
+    //登录
+    async function login() {
+        if (userName.current.value.length == 0) { alert('请输入登录名'); return; }
+        else if (verifyCode.current && verifyCode.current.value.length == 0) { alert('请输入验证码'); return; }
+        let obj = {
+            userName: userName.current.value,
+            userPwd: userPwd.current.value,
+            verifyCode: verifyCode.current ? verifyCode.current.value : '',
+        }
+        let msg = await axios.post("/api/common/login", obj);
+        if (msg.code == 1) {
+            //记住密码
+            if (remember.current && remember.current.checked) {
+                common.setLocalStorage("rememberUserName", userName.current.value);
+                common.setLocalStorage("rememberUserPwd", userPwd.current.value);
+            } else {
+                common.setLocalStorage("rememberUserName", '');
+                common.setLocalStorage("rememberUserPwd", '');
+            }
+            //登录成功之后，解析 返回的 token成json，保存在本地
+            let tokenArr = msg.data.split('.');
+            let loginMsg = JSON.parse(window.atob(tokenArr[1]));
+            loginMsg.token = msg.data;
+            common.setSessionStorage("loginMsg", loginMsg);
+            navigate('/admin');
+        } else if (msg.code == -2) {
+            alert(msg.msg);
+            verifyImg.current && verifyImg.current.click();//刷新验证码
+            setIsVerifyCode(true);
+        }
+    }
 
 
 
     return (
         <div className="plr-15 flex-center" style={{ height: '100vh', backgroundColor: '#E9ECEF' }}>
             <div className="max-w-100 bg-white plr-30 ptb-30 mlr-auto" style={{ width: '360px', boxShadow: '0 0 1px rgb(0 0 0 / 13%), 0 1px 3px rgb(0 0 0 / 20%)' }}>
-                <div className="font-bold font-16 text-center" style={{ color :'#666'}} >后台管理中心-登录</div>
-                <label className="br-4 plr-10 mt-20 flex-center" style={{ height: '38px', border: '1px solid #CED4DA' }}><input type="text" className="border-none flex-1" placeholder="登录名" /><i className="icon-user font-16"></i></label>
-                <label className="br-4 plr-10 mt-20 flex-center" style={{ height: '38px', border: '1px solid #CED4DA' }}><input type="text" className="border-none flex-1" placeholder="密码" /><i className="icon-lock font-16"></i></label>
-                <div className="br-4 plr-10 mt-20 flex-center" style={{ height: '38px', border: '1px solid #CED4DA' }}><input type="text" className="border-none flex-1 h-100" placeholder="验证码" /><img src="" title="看不清，换一张" /></div>
-                <label className="mt-20 inline-block"><input type="checkbox" className="vertical-middle" /><span className="vertical-middle">记住密码</span></label>
-                <button className="w-100 mt-20 ptb-10 border-none br-4 color-white" style={{ backgroundColor: '#007BFF' }}>登录</button>
+                <div className="font-bold font-16 text-center" style={{ color: '#666' }} >后台管理中心-登录</div>
+                <label className="br-4 plr-10 mt-20 flex-center" style={{ height: '38px', border: '1px solid #CED4DA' }}><input type="text" className="border-none flex-1" placeholder="登录名" ref={userName} onKeyUp={(e) => e.keyCode == 13 && login()} /><i className="icon-user font-16"></i></label>
+                <label className="br-4 plr-10 mt-20 flex-center" style={{ height: '38px', border: '1px solid #CED4DA' }}><input type="password" className="border-none flex-1" placeholder="密码" ref={userPwd} onKeyUp={(e) => e.keyCode == 13 && login()} /><i className="icon-lock font-16"></i></label>
+                {isVerifyCode && <div className="br-4 plr-10 mt-20 flex-center" style={{ height: '38px', border: '1px solid #CED4DA' }}><input type="text" className="border-none flex-1 h-100" placeholder="验证码" ref={verifyCode} onKeyUp={(e) => e.keyCode == 13 && login()} /><img src="/api/common/verifyCode" ref={verifyImg} onClick={(e) => e.target.src = common.baseURL + '/api/common/verifyCode?id=' + Math.random()} title="看不清？换一张" /></div>}
+                <label className="mt-20 inline-block"><input type="checkbox" className="vertical-middle" ref={remember} /><span className="vertical-middle">记住密码</span></label>
+                <button className="w-100 mt-20 ptb-10 border-none br-4 color-white" style={{ backgroundColor: '#007BFF' }} onClick={() => login()}>登录</button>
             </div>
         </div>
     );
---
+++ UI/Controllers/Api/CommonController.cs
@@ -0,0 +1,60 @@
+﻿using DbOrm;
+using DbOrm.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace UI.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CommonController : ControllerBase
+    {
+        [HttpPost]
+        public Result Login(object param)
+        {
+            dynamic jObj = JObject.Parse(param.ToString());
+            string userName = jObj.userName;
+            string userPwd = jObj.userPwd;
+            string verifyCode = jObj.verifyCode;
+            //验证验证码
+            int errorCount = Convert.ToInt32(Base.GetCache("LoginCount" + userName));
+            if (errorCount >= 3)
+            {
+                if (verifyCode == null || verifyCode.Length == 0 || verifyCode.ToUpper() != (string)Base.GetCache("VerifyCode" + HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString()))
+                {
+                    return Result.Fail("验证码错误", -2);
+                }
+            }
+            //验证用户名密码
+            List<Sys_Admin> list = DB.Query<Sys_Admin>("select * from Sys_Admin where UserName=@UserName and UserPwd=@UserPwd", new { UserName = userName, UserPwd = userPwd == null ? "" : Base.Encry(userPwd) });
+            if (list.Count > 0)
+            {
+                Sys_Admin admin = list[0];
+                string token = JWT.Signature(new
+                {
+                    ID = admin.ID,
+                    UserName = admin.UserName
+                });
+                return Result.OK(token);
+            }
+            else
+            {
+                errorCount = errorCount + 1;
+                Base.SetCache("LoginCount" + userName, errorCount, 5, false);
+                if (errorCount >= 3)
+                {
+                    return Result.Fail("用户名或密码错误", -2);
+                }
+                return Result.Fail("用户名或密码错误");
+            }
+        }
+
+        public FileResult VerifyCode()
+        {
+            string code = Common.VerifyCode.RandomStr(4);//获取随机数
+            Base.SetCache("VerifyCode" + HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString(), code, 5, false);//验证码存储起来
+            return File(Common.VerifyCode.CreateImage(code), "image/Jpeg");//生成图片并输出到客户端
+        }
+    }
+}
