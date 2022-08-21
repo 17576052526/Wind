a0a8559dee51abe5fa495abf95ee8702c73b37ce
17576052526 <727863984@qq.com>
2022-08-21 15:23:17 +0800

    -

--- clientApp/src/admin/importShare.js
+++ clientApp/src/admin/importShare.js
@@ -2,4 +2,27 @@
 import '../_plugins/globalCss/global.css'
 import '../_plugins/font/css/fontello.css'
 import '../_plugins/AdminUI2/UI.css'
+
 import '../_plugins/jqueryPluginInit'  //在所有 jquery插件之前引入
+
+import '../_plugins/alert/alert.css'
+import '../_plugins/alert/alert.js'
+
+import '../_plugins/box-move/box-move.css'
+import '../_plugins/box-move/box-move.js'
+
+import '../_plugins/box-resize/box-resize.js'
+
+import '../_plugins/confirm/confirm.css'
+import '../_plugins/confirm/confirm.js'
+
+import '../_plugins/table-fixed/table-fixed.css'
+import '../_plugins/table-fixed/table-fixed.js'
+
+import '../_plugins/table-resize/table-resize.css'
+import '../_plugins/table-resize/table-resize.js'
+
+import '../_plugins/upload/upload.js'
+
+import '../_plugins/verify/verify.css'
+import '../_plugins/verify/verify.js'
