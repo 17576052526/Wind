9d1f5151342c743fb94c4b014d53f7982bdaa3fd
17576052526 <727863984@qq.com>
2022-08-30 10:51:11 +0800

    -

--- UI/Controllers/Api/CommonController.cs
+++ UI/Controllers/Api/CommonController.cs
@@ -3,6 +3,7 @@ using DbOrm.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
+using System.Text;
 
 namespace UI.Controllers.Api
 {
@@ -10,6 +11,36 @@ namespace UI.Controllers.Api
     [ApiController]
     public class CommonController : ControllerBase
     {
+        //_hostEnvironment.WebRootPath;//wwwroot文件夹的绝对路径
+        //_hostEnvironment.ContentRootPath;//当前项目的绝对路径
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public CommonController(IWebHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        //文件上传，可以接收多个上传的文件
+        public async Task<Result> Upload()
+        {
+            string path = _hostEnvironment.WebRootPath;//wwwroot文件夹的绝对路径
+            //构造保存目录
+            string direct = "/upload/" + DateTime.Now.ToString("yyyyMMdd/");
+            if (!System.IO.Directory.Exists(path + direct)) { System.IO.Directory.CreateDirectory(path + direct); } //不存在目录就创建目录
+            //遍历文件
+            StringBuilder filePaths = new StringBuilder();
+            foreach (var item in Request.Form.Files)
+            {
+                string fileExt = item.FileName.Substring(item.FileName.LastIndexOf('.')); //文件后缀名
+                string newFileName = Convert.ToInt64((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds).ToString() + (new Random().Next(100000, 1000000)) + fileExt;  //构造文件名，规则：时间戳+随机数6位
+                using (var stream = new FileStream(path + direct + newFileName, FileMode.Create))
+                {
+                    await item.CopyToAsync(stream);
+                }
+                filePaths.Append(direct + newFileName + '|');
+            }
+            return Result.OK(filePaths.ToString().TrimEnd('|'));
+        }
+
         [HttpPost]
         public Result Login(object param)
         {
