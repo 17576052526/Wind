f4d41722906b1d1a981e5276fb364eec61c27578
17576052526 <727863984@qq.com>
2023-03-22 18:02:02 +0800

    -

--- clientapp/src/index.js
+++ clientapp/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    //禁用严格模式，因为开发环境下 useEffect 会调用两次，打包后不会调用两次
+    //<React.StrictMode>
     <App />
-  </React.StrictMode>
+    //</React.StrictMode>
 );
