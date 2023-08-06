a9cf75707d77862b3df6fba427c6bcdee7015ca6
17576052526 <727863984@qq.com>
2023-08-06 15:23:02 +0800

    -

---
+++ Plugins/wwwroot/UIPlugins/scroll-show/scroll-show.js
@@ -0,0 +1,11 @@
+﻿function scrollShow() {
+    var scrollTop = document.documentElement.scrollTop;
+    var height = $(window).height();
+    $('.scroll-show').each(function () {
+        if (scrollTop + height > $(this).offset().top) {
+            $(this).removeClass('scroll-show').addClass($(this).attr('addClass'));
+        }
+    })
+}
+$(scrollShow);
+$(window).on('scroll', scrollShow);
