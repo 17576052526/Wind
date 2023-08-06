2ba990cf469aca15112d1d958505b293e123d3bd
17576052526 <727863984@qq.com>
2023-08-06 17:09:12 +0800

    -

--- Plugins/wwwroot/UIPlugins/scroll-show/scroll-show.js
+++ Plugins/wwwroot/UIPlugins/scroll-show/scroll-show.js
@@ -1,9 +1,17 @@
 ﻿function scrollShow() {
     var scrollTop = document.documentElement.scrollTop;
     var height = $(window).height();
-    $('.scroll-show').each(function () {
-        if (scrollTop + height > $(this).offset().top) {
-            $(this).removeClass('scroll-show').addClass($(this).attr('addClass'));
+    $('[scroll-animate]').each(function () {
+        var th = $(this);
+        if (scrollTop + height > th.offset().top) {
+            var delay = th.attr('delay');
+            if (delay) {
+                setTimeout(function () {
+                    th.addClass(th.attr('scroll-animate')).removeAttr('scroll-animate');
+                }, parseFloat(delay) * 1000)
+            } else {
+                th.addClass(th.attr('scroll-animate')).removeAttr('scroll-animate');
+            }
         }
     })
 }
