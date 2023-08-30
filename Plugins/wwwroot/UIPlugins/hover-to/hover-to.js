3a9ac3a8f4e425ba00808ac0200f5e278e2e91c3
17576052526 <727863984@qq.com>
2023-08-30 14:09:05 +0800

    -

---
+++ Plugins/wwwroot/UIPlugins/hover-to/hover-to.js
@@ -0,0 +1,12 @@
+﻿$(document).on('mouseenter', '[hover-to]', function () {
+    var str = $(this).attr('hover-to')
+    var obj = str ? eval(str) : $(this).find('[in-add],[in-remove]')
+    obj.addClass(obj.attr('in-add'))
+    obj.removeClass(obj.attr('in-remove'))
+})
+$(document).on('mouseleave', '[hover-to]', function () {
+    var str = $(this).attr('hover-to')
+    var obj = str ? eval(str) : $(this).find('[out-add],[out-remove]')
+    obj.addClass(obj.attr('out-add'))
+    obj.removeClass(obj.attr('out-remove'))
+})
