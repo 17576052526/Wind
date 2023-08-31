9074ce7ff80b914a5c88db51c404930083c5e646
17576052526 <727863984@qq.com>
2023-08-31 14:03:15 +0800

    -

---
+++ Plugins/wwwroot/UIPlugins/click-to/click-to.js
@@ -0,0 +1,8 @@
+﻿$(document).on('click', '[click-to]', function () {
+    var str = $(this).attr('click-to')
+    var obj = str ? eval(str) : $(this).find('[add],[remove]')
+    obj.each(function () {
+        $(this).addClass($(this).attr('add'))
+        $(this).removeClass($(this).attr('remove'))
+    })
+})
