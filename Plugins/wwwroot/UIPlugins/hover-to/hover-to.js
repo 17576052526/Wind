7f58ef9bf9ca5cb5b6bf1cdb3307df527b2a1242
17576052526 <727863984@qq.com>
2023-08-31 14:02:43 +0800

    -

--- Plugins/wwwroot/UIPlugins/hover-to/hover-to.js
+++ Plugins/wwwroot/UIPlugins/hover-to/hover-to.js
@@ -1,12 +1,16 @@
 ﻿$(document).on('mouseenter', '[hover-to]', function () {
     var str = $(this).attr('hover-to')
     var obj = str ? eval(str) : $(this).find('[in-add],[in-remove]')
-    obj.addClass(obj.attr('in-add'))
-    obj.removeClass(obj.attr('in-remove'))
+    obj.each(function () {
+        $(this).addClass($(this).attr('in-add'))
+        $(this).removeClass($(this).attr('in-remove'))
+    })
 })
 $(document).on('mouseleave', '[hover-to]', function () {
     var str = $(this).attr('hover-to')
     var obj = str ? eval(str) : $(this).find('[out-add],[out-remove]')
-    obj.addClass(obj.attr('out-add'))
-    obj.removeClass(obj.attr('out-remove'))
+    obj.each(function () {
+        $(this).addClass($(this).attr('out-add'))
+        $(this).removeClass($(this).attr('out-remove'))
+    })
 })
