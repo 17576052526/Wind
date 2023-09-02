72f513ac8ce8e59e6db88f18caa74aa0fc789b5f
17576052526 <727863984@qq.com>
2023-09-02 14:48:02 +0800

    -

---
+++ Plugins/wwwroot/UIPlugins/scroll-to/scroll-to.js
@@ -0,0 +1,16 @@
+﻿var __scroll_x = document.documentElement.scrollTop || document.body.scrollTop;
+function scrollAnimate() {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var height = $(window).height();
+    $('[scroll-in-add],[scroll-in-remove],[scroll-out-add],[scroll-out-remove]').each(function () {
+        var th = $(this);
+        if (__scroll_x > scrollTop && th.offset().top + th.height() > scrollTop + height) {
+            th.addClass(th.attr('scroll-out-add')).removeClass(th.attr('scroll-out-remove'));
+        } else if (th.offset().top < scrollTop + height) {
+            th.addClass(th.attr('scroll-in-add')).removeClass(th.attr('scroll-in-remove'));
+        }
+    })
+    __scroll_x = scrollTop;
+}
+$(scrollAnimate);
+$(window).on('scroll', scrollAnimate);
