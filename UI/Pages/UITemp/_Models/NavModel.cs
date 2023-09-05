05a4be92438de72a5f00dce5eb9e73371037db71
17576052526 <727863984@qq.com>
2023-09-05 20:39:31 +0800

    -

---
+++ UI/Pages/UITemp/_Models/NavModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UI.Pages.UITemp._Models
+{
+    public class NavModel
+    {
+        public string Name;
+        public string Url;
+        public string Target;
+        public List<NavModel> Children;
+    }
+}
