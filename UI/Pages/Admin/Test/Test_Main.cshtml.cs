5fc846dc21826208e9d7814d78c73a5f34b8b1a0
17576052526 <727863984@qq.com>
2023-09-09 19:47:58 +0800

    -

--- UI/Pages/Admin/Test/Test_Main.cshtml.cs
+++ UI/Pages/Admin/Test/Test_Main.cshtml.cs
@@ -9,57 +9,24 @@ namespace UI.Pages.Admin
 {
     public class Test_MainModel : ListPageModel
     {
-        //查询条件
-        private string _where;
-        private string Where
-        {
-            get
-            {
-                if (_where == null)
-                {
-                    _where += !String.IsNullOrEmpty(Request.Query["MainID"]) ? " and Test_Main.MainID like @MainID" : null;
-                    _where += !String.IsNullOrEmpty(Request.Query["Sys_Type_ID"]) ? " and Test_Main.Sys_Type_ID = @Sys_Type_ID" : null;
-                    _where += !String.IsNullOrEmpty(Request.Query["CreateTime1"]) ? " and Test_Main.CreateTime >= @CreateTime1" : null;
-                    _where += !String.IsNullOrEmpty(Request.Query["CreateTime2"]) ? " and Test_Main.CreateTime <= @CreateTime2" : null;
-
-                    _where = Regex.Replace(_where, @"^\s*and", "", RegexOptions.IgnoreCase);
-                }
-                return _where;
-            }
-        }
-        //查询条件参数
-        private object _param;
-        private object Param
-        {
-            get
-            {
-                if (_param == null)
-                {
-                    _param = new
-                    {
-                        MainID = "%" + Request.Query["MainID"].ToString() + "%",
-                        Sys_Type_ID = Request.Query["Sys_Type_ID"].ToString(),
-                        CreateTime1 = Request.Query["CreateTime1"].ToString(),
-                        CreateTime2 = Request.Query["CreateTime2"].ToString(),
-                    };
-                }
-                return _param;
-            }
-        }
         public List<Test_Main> List;
         public void OnGet()
         {
-            //取数
             using (DB db = new DB())
             {
-                var sql = db.Select<Test_Main>("count(*)")
-                    .LeftJoin<Sys_Type>("Test_Main.Sys_Type_ID=Sys_Type.ID")
-                    .Where(Where, Param);
-                this.DataCount = Convert.ToInt32(sql.QueryScalar());
+                var sql = db.Select<Test_Main>("count(*)");
+                //构造条件，注意参数赋值要转类型
+                if (Request.Query["MainID"].ToString().Length > 0) { sql.WhereAnd("MainID like @MainID", new { MainID = '%' + Request.Query["MainID"] + '%' }); }
 
-                sql = sql.OrderBy("Test_Main.ID Desc");
-                this.List = sql.Select("Test_Main.*,Sys_Type.Name")
-                    .Query((PageIndex - 1) * PageSize, PageSize);
+                //表连接查询
+                sql.LeftJoin<Sys_Type>("Test_Main.Sys_Type_ID=Sys_Type.ID");
+                //查询总数据量
+                this.DataCount = Convert.ToInt32(sql.QueryScalar());
+                //设置查询列
+                sql.Select("Test_Main.*,Sys_Type.Name");
+                //排序
+                sql.OrderBy("Test_Main.ID desc");
+                this.List = sql.Query((PageIndex - 1) * PageSize, PageSize);
             }
         }
 
