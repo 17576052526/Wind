49dcd69fef012f16789b1d0ad2221cd12db25cbc
17576052526 <727863984@qq.com>
2023-09-16 13:00:51 +0800

    -

---
+++ UI/Pages/Admin/Sys/Temp_List1_Content.cshtml.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DbOrm;
+using DbOrm.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Pages.Admin
+{
+    public class Temp_List1_ContentModel : ListPageModel
+    {
+        public List<Temp_List1> List;
+        public void OnGet()
+        {
+            using (DB db = new DB())
+            {
+                var sql = db.Select<Temp_List1>("count(*)");
+                //构造条件
+                if (Request.Query["Name"].ToString().Length > 0) { sql.WhereAnd("Name like @Name", new { Name = '%' + Request.Query["Name"] + '%' }); }
+                sql.WhereAnd("TypeID = @TypeID", new { TypeID = Request.Query["typeID"].ToString() });
+
+                //查询总数据量
+                this.DataCount = Convert.ToInt32(sql.QueryScalar());
+                //设置查询列
+                sql.Select("Temp_List1.*");
+                //排序
+                sql.OrderBy("Temp_List1.ID desc");
+                this.List = sql.Query((PageIndex - 1) * PageSize, PageSize);
+            }
+        }
+
+        //删除，在页面头部加上 @page "{handler}"，/list/Delete 就可访问，否则要 /list?handler=Delete才能访问
+        public IActionResult OnPostDelete()
+        {
+            using (DB db = new DB())
+            {
+                db.BeginTransaction();
+                try
+                {
+                    foreach (var m in Request.Form["checkID"])
+                    {
+                        db.Delete<Temp_List1>("ID=@ID", new { ID = m });
+                    }
+                    db.CommitTransaction();
+                }
+                catch { db.RollbackTransaction(); throw; }
+            }
+            return Redirect(Request.Path.Value.Remove(Request.Path.Value.LastIndexOf('/')) + Request.QueryString);
+        }
+
+    }
+}
