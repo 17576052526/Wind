688b2bfcdea08379aa1573c799dd9c084952a4b9
17576052526 <727863984@qq.com>
2023-09-22 14:41:39 +0800

    -

--- UI/Pages/Admin/Index.cshtml.cs
+++ UI/Pages/Admin/Index.cshtml.cs
@@ -9,23 +9,9 @@ namespace UI.Pages.Admin
     [Authorize]//验证是否有登录
     public class IndexModel : PageModel
     {
-        public List<Sys_Type> List;
         public void OnGet()
         {
-            using (DB db = new DB())
-            {
-                var sql = db.Select<Sys_Type>();
-                //构造条件
-                sql.WhereAnd("TypeID = @TypeID", new { TypeID = 0 });
-                this.List = sql.OrderBy("Sort asc").Query();
 
-                //查询子集
-                foreach (var m in List)
-                {
-                    m.Children = db.Select<Sys_Type>().Where("TypeID=@TypeID", new { TypeID = m.ID }).OrderBy("Sort asc").Query();
-
-                }
-            }
         }
     }
 }
