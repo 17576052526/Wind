d7f729710017dfb70c265c791c275cf16c3bd7c2
17576052526 <727863984@qq.com>
2023-12-13 13:06:40 +0800

    -

--- Plugins/wwwroot/UIPlugins/banner-slide/banner-slide.js
+++ Plugins/wwwroot/UIPlugins/banner-slide/banner-slide.js
@@ -1,107 +1,83 @@
-﻿$.fn.bannerSlide = function (settings) {
-    var param = $.extend({
-        interval: 5000,   //间隔5秒切换一次
-        speed: 500,    //0.5秒切换完成
-        switchStop: 30000,    //手动切换后暂停多久重新启动计时器
-    }, settings || {});
-
-    var box = this.children().eq(0);
-    var li = box.children();
+﻿//interval 切换的间隔，speed 切换的速度，switchStop 手动切换后暂停时间
+$.fn.bannerSlide = function (interval, speed, switchStop) {
+    var box = this.children('.banner-slide-box');
+    var items = box.children();
     //左右各加一个
-    box.prepend(li.eq(li.length - 1).clone());
-    box.append(li.eq(0).clone());
-    //第一个添加类样式（动画）
-    li.eq(0).find('[animate]').each(function () {
-        $(this).addClass($(this).attr('animate'));
-    });
+    box.prepend(items.eq(items.length - 1).clone());
+    box.append(items.eq(0).clone());
     //设置第一个显示
     box.css('transform', 'translateX(-100%)');
-    var index = 1;
     //设置过渡效果
-    setTimeout(function () { box.css('transition', 'all ' + (param.speed / 1000) + 's'); }, 100);
+    setTimeout(function () { box.css('transition', 'all ' + speed + 'ms'); }, 100);
     //生成按钮
     var btnBox = $('<div class="banner-slide-btnBox"></div>').appendTo(this);
-    for (var i = 0; i < li.length; i++) {
+    for (var i = 0; i < items.length; i++) {
         btnBox.append('<div class="banner-slide-btn"></div>')
     }
     btnBox.children().eq(0).addClass('banner-slide-btnActive');
     btnBox.on('click', '.banner-slide-btn', function () {
-        index = $(this).index() + 1;
-        exec();
-        ztTime();//手动切换后暂停一段时间重新启动计时器
+        exec($(this).index() + 1);
+        handSwitch();
     });
-    //暂停一段时间后重新启动计时器
-    function ztTime() {
+    //第一个添加类样式（动画）
+    items.eq(0).find('[animate]').each(function () { $(this).addClass($(this).attr('animate')) });
+    //手动切换事件
+    function handSwitch() {
         clearInterval(time);
-        setTimeout(() => setTime(), param.switchStop);
+        setTimeout(() => setTime(), switchStop);
     }
-    //执行
-    function exec() {
-        box.css('transform', 'translateX(-' + index + '00%)');
+    //切换
+    var index = 1;
+    function exec(i) {
+        box.css('transform', 'translateX(-' + i + '00%)');
+        index = i >= items.length + 1 ? 1 : i <= 0 ? items.length : i;
         //动画完成后在执行判断
         setTimeout(function () {
-            if (index >= li.length + 1) {
-                index = 1;
-                box.css('transition-duration', '0s');
-                box.css('transform', 'translateX(-' + index + '00%)');
-                setTimeout(function () { box.css('transition-duration', (param.speed / 1000) + 's'); }, 100);
-            } else if (index <= 0) {
-                index = li.length;
-                box.css('transition-duration', '0s');
-                box.css('transform', 'translateX(-' + index + '00%)');
-                setTimeout(function () { box.css('transition-duration', (param.speed / 1000) + 's'); }, 100);
+            if (i >= items.length + 1 || i <= 0) {
+                box.css({ 'transition-duration': '0ms', 'transform': 'translateX(-' + index + '00%)' });
+                setTimeout(function () { box.css('transition-duration', speed + 'ms'); }, 100);
             }
             //按钮选中
             btnBox.children().removeClass('banner-slide-btnActive');
             btnBox.children().eq(index - 1).addClass('banner-slide-btnActive');
-            //其他的去掉类样式
-            li.find('[animate]').each(function () {
-                $(this).removeClass($(this).attr('animate'));
-            });
-            //切换完后子节点可以添加类样式（动画）
-            li.eq(index - 1).find('[animate]').each(function () {
-                $(this).addClass($(this).attr('animate'));
-            });
-        }, param.speed);
+            //动画显示
+            items.find('[animate]').each(function () { $(this).removeClass($(this).attr('animate')); });
+            items.eq(index - 1).find('[animate]').each(function () { $(this).addClass($(this).attr('animate')); });
+        }, speed);
     }
     //计时器
     var time;
     function setTime() {
         clearInterval(time);
-        time = setInterval(function () {
-            index++;
-            exec();
-        }, param.interval)
+        time = setInterval(function () { exec(index + 1) }, interval);
     }
     setTime();
     //上一个
     function previous() {
-        index--;
-        exec();
-        ztTime();//手动切换后暂停一段时间重新启动计时器
+        exec(index - 1);
+        handSwitch();
     }
     //下一个
     function next() {
-        index++;
-        exec();
-        ztTime();//手动切换后暂停一段时间重新启动计时器
+        exec(index + 1);
+        handSwitch();
     }
     //移动端左右滑动事件
     var startX, moveX, width = this.width()
-    this.get(0).addEventListener('touchstart', function (event) {
-        startX = event.touches[0].pageX
+    this.on('touchstart', function (e) {
+        startX = e.touches[0].pageX;
         clearInterval(time);//停止计时器
-        box.css('transition-duration', '0s');//停止过渡效果
+        box.css('transition-duration', '0ms');//停止过渡效果
     })
-    this.get(0).addEventListener('touchmove', function (event) {
-        moveX = event.touches[0].pageX - startX
+    this.on('touchmove', function (e) {
+        moveX = e.touches[0].pageX - startX;
         box.css('transform', 'translateX(-' + (width * index - moveX) + 'px)');//移动
     })
-    this.get(0).addEventListener('touchend', function (event) {
+    this.on('touchend', function (e) {
         //重新启动计时器
         setTime();
         //启动过渡效果
-        box.css('transition-duration', (param.speed / 1000) + 's');
+        box.css('transition-duration', speed + 'ms');
 
         if (moveX < -80) {
             next();
@@ -112,5 +88,27 @@
         }
     })
 
-    return [previous, next, (a) => a ? setTime() : clearInterval(time)];//上一个，下一个
+    return [previous, next, (a) => a ? setTime() : clearInterval(time)];
 }
+
+$(function () {
+    //启动方法，可以在启动方法里面写扩展
+    function start(th) {
+        let [previous, next, startOrPause] = th.bannerSlide(parseInt(th.attr('interval')), parseInt(th.attr('speed')), parseInt(th.attr('switchStop')));
+
+    }
+    //同时支持数据后端渲染，和ajax渲染
+    $('.banner-slide').each(function () {
+        let th = $(this);
+        if (th.children('.banner-slide-box').children().length > 0) {
+            start(th);
+        } else {
+            let time = setInterval(function () {
+                if (th.children('.banner-slide-box').children().length > 0) {
+                    start(th);
+                    clearInterval(time);
+                }
+            }, 1000)
+        }
+    });
+})
