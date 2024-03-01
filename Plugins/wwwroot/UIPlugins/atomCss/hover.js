0f24d62a379aa168ab8bd7dd9e638960a0ac9ef8
17576052526 <727863984@qq.com>
2024-03-01 23:57:49 +0800

    -

---
+++ Plugins/wwwroot/UIPlugins/atomCss/hover.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('mouseenter', function (e) {
+    if (!e.target.hasAttribute || !e.target.hasAttribute('hover')) { return }
+    var cur = e.target;
+    cur.setAttribute('__hover-class', cur.className);
+    cur.getAttribute('hover').split(' ').forEach(s => cur.classList.add(s));
+}, true);
+document.addEventListener('mouseleave', function (e) {
+    if (!e.target.hasAttribute || !e.target.hasAttribute('hover')) { return }
+    var cur = e.target;
+    cur.className = cur.getAttribute('__hover-class');
+}, true);
---
+++ Plugins/wwwroot/UIPlugins/atomCss/scroll-to.js
@@ -0,0 +1,30 @@
+﻿var __previousScroll = document.documentElement.scrollTop || document.body.scrollTop;
+function __scrollTo() {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var height = document.documentElement.clientHeight;
+    document.querySelectorAll('[scroll-in-add],[scroll-in-remove],[scroll-out-add],[scroll-out-remove]').forEach(cur => {
+        var obj = cur.getBoundingClientRect();
+        var top = obj.top + scrollTop;//绝对位置
+        if (__previousScroll > scrollTop && top + obj.height > scrollTop + height) {
+            var outAdd = cur.getAttribute('scroll-out-add');
+            var outRemove = cur.getAttribute('scroll-out-remove');
+            outAdd && outAdd.split(' ').forEach(s => cur.classList.add(s));
+            outRemove && outRemove.split(' ').forEach(s => cur.classList.remove(s));
+        } else if (top < scrollTop + height) {
+            var inAdd = cur.getAttribute('scroll-in-add');
+            var inRemove = cur.getAttribute('scroll-in-remove');
+            inAdd && inAdd.split(' ').forEach(s => cur.classList.add(s));
+            inRemove && inRemove.split(' ').forEach(s => cur.classList.remove(s));
+        }
+    })
+    __previousScroll = scrollTop;
+}
+window.addEventListener('load', __scrollTo);
+//减少代码执行频率
+var __scroll_is;
+window.addEventListener('scroll', function () {
+    if (!__scroll_is) {
+        __scroll_is = true;
+        setTimeout(() => { __scroll_is = false; __scrollTo(); },50);
+    }
+})
