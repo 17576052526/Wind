823f371338bbee35d69a353ceceabf48f4dfd804
17576052526 <727863984@qq.com>
2025-02-19 14:59:55 +0800

    -

---
+++ Plugins/wwwroot/UIPlugins/click/click.js
@@ -0,0 +1,35 @@
+document.addEventListener('click', function (e) {
+    if (!e.target.closest('[click-trigger]')) { return }
+    var cur = e.target.closest('[click-trigger]');
+    var box = cur.closest('[click]') || cur;
+
+    var mark = box.getAttribute('_click-mark') != 'in' ? 'in' : '';
+    box.setAttribute('_click-mark', mark);
+
+    function addClassByNode(node) {
+        let inClass = node.getAttribute('click-in');
+        let outClass = node.getAttribute('click-out');
+        if (inClass || outClass) {
+            if (mark) {
+                inClass && node.classList.add(inClass)
+                outClass && node.classList.remove(outClass)
+            } else {
+                outClass && node.classList.add(outClass)
+                inClass && node.classList.remove(inClass)
+            }
+        }
+    }
+
+    //递归查找子集，此处不能用querySelectorAll，因为子集中如果有 click 就不能在往里面找了
+    function find(box) {
+        for (let child of box.children) {
+            if (child.hasAttribute('click')) { continue; }
+            addClassByNode(child);
+            find(child);
+        }
+    }
+    find(box)
+
+    //自身
+    addClassByNode(box);
+});
